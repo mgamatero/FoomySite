cfaa639d496be98764e80211aa73856660c4cf65
Mike Gamatero <mgamatero@gmail.com>
2020-01-09 12:06:00 -0800

    login page added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+import {PasswordModule} from 'primeng/password';
 
 
 @Component({
