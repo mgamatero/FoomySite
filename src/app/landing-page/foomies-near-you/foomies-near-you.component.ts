6a89d8b2df6aaa15409eefe81a29e9dd4c6c6419
Mike Gamatero <mgamatero@yahoo.com>
2020-02-15 12:01:00 -0800

    landing page, conditional rendering for foomies near you.  hardcoded cards if not logged in, firebase cards if logged in

--- src/app/landing-page/foomies-near-you/foomies-near-you.component.ts
+++ src/app/landing-page/foomies-near-you/foomies-near-you.component.ts
@@ -14,27 +14,36 @@ export class FoomiesNearYouComponent implements OnInit {
   foommyFromDB: any;
   foommyinfo: FoommyInfo;
 
+  hardCodedFoommy = [
+    {
+      chef_image:
+        "http://www.vancouversun.com/life/cms/binary/6368589.jpg?size=640x420",
+      chef_name: "Marvin's Kitchen",
+      chef_city: "Tustin",
+      chef_state: "CA",
+      chef_description:
+        "Bringing my childhood favorites to your world.  I specialize mostly in Taiwanese cuisine, with a few suprises here and there.  Come see for yourself!"
+    },
+    {
+      chef_image:
+        "http://cook.fnr.sndimg.com/content/dam/images/cook/fullset/2010/5/21/1/generic_italian-pantry_s4x3.jpg.rend.hgtvcom.1280.960.suffix/1357774373206.jpeg",
+      chef_name: "Tavolo Per Piatto",
+      chef_city: "Fullerton",
+      chef_state: "CA",
+      chef_description:
+        "Fresh Italian food as cooked by my Grandma. As Italian as you can get."
+    }
+  ];
+
   constructor(
     private foommyService: FoomiesService,
     public afAuth: AuthService
   ) {}
 
   ngOnInit() {
-    // const x: FoommyInfo = {
-    //   title: "BaoBaoBao",
-    //   address: "33442 Baoland, Bao CA 99080",
-    //   image:
-    //     "https://tse2.mm.bing.net/th?id=OIP.QZNoUBMEJv51qOi02cczoAHaE7&pid=Api&P=0&w=265&h=178",
-    //   specialties: "Bao"
-    // };
-    // this.foommyService.addFoommies(x);
-
-    //ONLY USE FIREBASE IF LOGGED IN - INJECT HARDCODED DATA HERE
-    if (this.afAuth.loggedIn) {
-      this.foommyService.getFoommies().subscribe(foommy => {
-        // console.log(foommy)
-        this.foommyFromDB = foommy;
-      });
-    }
+    //ONLY USE FIREBASE IF LOGGED IN
+    this.foommyService.getFoommies().subscribe(foommy => {
+      this.foommyFromDB = foommy;
+    });
   }
 }
