e307d6980b01ddd9ac50276da85595305f75ea1f
Mike Gamatero <mgamatero@yahoo.com>
2020-02-17 10:05:29 -0800

    fix login form -- reactive (fully, no more ngModel)

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -42,7 +42,7 @@ export class LoginComponent implements OnInit {
 
   onEmailLogIn() {
     console.log("Email Login");
-    this.afAuth.emailLogin(this.email,this.password)
+    this.afAuth.emailLogin(this.loginForm.value.email,this.loginForm.value.password)
       // .signInWithEmailAndPassword(this.email, this.password)
       .then(success => {
         console.log("Success email");
